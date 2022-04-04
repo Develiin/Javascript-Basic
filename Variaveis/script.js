d5e8c05c2615d8e5e5e216c71073cf2eddb80330
Évelin Thainá <86165618+Develiin@users.noreply.github.com>
2022-04-04 20:57:47 -0300

    Add files via upload

---
+++ Variaveis/script.js
@@ -0,0 +1,16 @@
+var a ;
+
+var b = "Hd";
+b=a; 
+a = "Placa de Captura"; 
+
+console.log(b); 
+
+// o sinal = Significa RECEBE e se lê recebe
+
+// Pode ser usado  na variavel nomes com: letras,números (nunca no inicio da palavra) ,underline e cifrão($) que é melhor não usar para não confundir futuramente em outras linguagens de programação. 
+// Se for uma palavra composta como por Ex: Caixa grande pode ser usado da seguinte forma caixaGrande, caixa_grande. Por PADRÃO sempre recomenda-se usar a primeira letra da palavra minuscula Ex: caixa 
+
+// No JAVASCRIPT letra maiúscula e minúscula são diferente. 
+// "a" é diferente de "A" então se declarar uma variável como: caixaGrande e depois tentar usar ela digitando: caixagrande
+// Não irá funcionar pois para ele "caixaGrande e "caixagrande" Não são a mesma coisa.
