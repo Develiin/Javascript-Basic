da90c2609383bad00d82c2a13d26db2079cef6d7
Évelin Thainá <86165618+Develiin@users.noreply.github.com>
2022-04-05 21:16:40 -0300

    Add files via upload

---
+++ Tipos de dados/script.js
@@ -0,0 +1,70 @@
+                        // Tipos de dados
+
+
+
+/*
+Boolean
+Null
+Undefined
+Number
+String
+ */
+
+        // String - 
+var a = "";
+
+console.log();
+
+// Quando se faz esse exemplo está se criando uma string. É uma cadeia de caracteres, elementos sendo assim conhecido por STRING
+// pode se ter caracteres especiais como o /N
+
+
+
+        //  Number
+ var a = 1;
+ var b = "1";
+
+ console.log(a);
+ console.log(b);
+
+ console.log("1" + "1"); 
+ console.log(abc + d);       
+    /* O que defini o que é STRING é as aspas " " 
+     no exemplo são duas strings somadas, se tem uma concatenação que é juntar as palavras formando uma palavra só, nesse caso somando o número 11  */
+
+    console.log(1+1); 
+    /* Nesse exemplo os dois numeros se somam, porque não tendo as aspas não é uma STRING e Sim NUMBER
+      que é utilizado quando se quiser fazer operações matemática.
+
+            Quando se quer identificar alguém nesse caso mesmo sendo números se utiliza STRING */
+
+
+
+        // Undefined
+
+var e = 1;
+
+console.log(e);
+
+// Undefined é quando não se inicializa a variavel e o programa não sabe o que tem ali dentro
+
+
+
+            // Null
+
+var f = null;
+
+console.log(f);
+
+// é  a certeza que não tem nada ali dentro, que aquela variavel está vazia.
+
+
+            // Booleon pode ser verdadeiro ou falso
+
+var g = true;
+var g = false;
+
+console.log(g);
+
+// Permite fazer operações lógicas, como por exemplo pode se verificar se alguma coisa é verdadeira ou não e tomar uma opção 
+
