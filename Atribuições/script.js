1c84bb26d675750b7295299edc5a8175ffb4096a
Évelin Thainá <86165618+Develiin@users.noreply.github.com>
2022-04-13 20:34:26 -0300

    Add files via upload

---
+++ Atribuições/script.js
@@ -0,0 +1,16 @@
+/*
+    =      X = Y     X = Y        ->  Todos os três são formas diferentes de fazer a mesma função  <-
+    +=     X += Y    X = X + Y    ->  Todos os três são formas diferentes de fazer a mesma função  <-
+    -=     X -= Y    X = X - Y    ->  Todos os três são formas diferentes de fazer a mesma função  <-
+    *=     X *= Y    X = X * Y    ->  Todos os três são formas diferentes de fazer a mesma função  <-
+    /=     X /= Y    X = X / Y    ->  Todos os três são formas diferentes de fazer a mesma função  <-
+    %=     X %= Y    X = X % Y    ->  Todos os três são formas diferentes de fazer a mesma função  <-
+
+
+*/
+
+var a = 30;
+
+a = a % 28;
+
+console.log(a);
---
+++ Operações aritméticas/script.js
@@ -0,0 +1,22 @@
+/*
+    + Adição
+    - Subtração
+    * Multiplicação
+    / Divisão
+    % Módulo
+    ++ Incremento
+    -- Decremento
+
+        Todos eles são programadores aritméticos e 
+        são usados juntos na programação
+*/
+
+var a = 10;
+var b = 15;
+
+var c = a % b;
+
+console.log(c);
+/* É possivel usar operações aritméticas sempre do lado direito 
+do igual para colocar nesse casopara colocar na varivel C */
+
