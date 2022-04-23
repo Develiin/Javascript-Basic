c5baa792ae29f61d296d86b2abd4cf97b2dc427e
Évelin Thainá <86165618+Develiin@users.noreply.github.com>
2022-04-23 20:41:35 -0300

    Add files via upload

---
+++ Operador Ternário/script.js
@@ -0,0 +1,14 @@
+ var idade = 16;
+
+ var pode = idade >= 18 ? "pode" : "Não Pode";
+
+ console.log(pode);
+
+ // o ponto de interrogação ? SIGNIFICA verdadeiro ou SE
+ 
+ // os DOIS PONTOS : significa FALSO  OU SE NÃO
+
+/*
+    Sempre será usado nessa ordem e é a mesma coisa que utilizar o IF e ELSE porém deixa o código mais limpo 
+
+*/
