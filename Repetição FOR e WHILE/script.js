bae52d528ffc033685da27026810fe897d46b258
Évelin Thainá <86165618+Develiin@users.noreply.github.com>
2022-04-27 20:35:12 -0300

    Add files via upload

---
+++ Repetição FOR e WHILE/script.js
@@ -0,0 +1,29 @@
+// var numero = 5;
+
+// for (var vez = 0; vez < numero; vez++) {
+
+//     console.log("Executando o for, pela " + vez + " Vez ");
+// }
+
+
+// console.log("Acabou");
+
+
+var numero = Math.random() * 100;
+
+while (numero < 90) {
+
+    console.log(numero);
+    numero = Math.random() * 100;
+
+}
+
+console.log(numero);
+console.log("Acabou");
+
+
+//  Significa enquanto (while) é um lupim infinito
+
+/*Sera usado o while quando tem uma condição que quer que seja satisfeita mas que a
+gente não sabe quando que ela vai ser satisfeita */  
+
