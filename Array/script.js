bf5c3151f0df358c42eb08ad9c4c056799ed1d95
Évelin Thainá <86165618+Develiin@users.noreply.github.com>
2022-04-28 21:33:00 -0300

    Add files via upload

---
+++ Array/script.js
@@ -0,0 +1,36 @@
+
+
+var alunos = ["Igor", "José", "Marcos", 
+    "Mariana", "Pedro", "João"]
+
+// for (var i = 0; i < alunos.length; i++) {
+//     console.log(alunos[i])
+// }
+
+for (var i in alunos) {               /* O For com o IN irá pegar o INDEX e com o OF irá pegar o valor mesmo o elemento daquele INDEX mesmo NADA MAIS É QUE UMA SEQUENCIA DE ELEMENTOS JUNTOS  */
+    console.log(i)
+}
+
+
+
+
+
+
+
+
+/*  
+    Cada elemento da Array deve ser separado por uma vírgula.
+
+    Todo Array começa no número zero e sempre no zero além de ser sequencial
+
+    Sempre incia no zero menos 1, devidamente à ele começar no zero
+
+    Por exemplo se quiser imprimir na tela apenas Marcos sera colcoado entre colchetes [2]
+    o número dois ja que começa no zero ele é o terceiro a aparecer mas por questão de número ele é o segundo  
+
+
+    Array é um conjunto de elementos, um conjunto de váriaveis com qualuer tipo de dados
+
+    Por ultimo ele sempre terá um length no final da lista quando imprimir
+*/
+
