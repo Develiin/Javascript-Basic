32aa7037135f46ee7d2a971c0df0b855a219ea9c
Évelin Thainá <86165618+Develiin@users.noreply.github.com>
2022-08-05 20:14:34 -0300

    Add files via upload

---
+++ Objetos e propriedades/script.js
@@ -0,0 +1,9 @@
+// Diferença do ARRAY para OBJETO É: No array é utilizado os colchotes[] No array pode ser acessado um elemento que está dentro dele através do Index
+// No Objeto é utilizado as Chaves {} é possivel criar uma chamada chave para acessar um elemento
+
+var aluno = {
+  nome: 'João',
+  nota1: 7.5
+}
+
+console.log(aluno.nota1)
